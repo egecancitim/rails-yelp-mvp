ba87d734f3fb772c52525cf7202c91cbb78e37aa
Egecan Citim <egecancitim@gmail.com>
2020-04-08 15:01:52 +0300

    rails yelp mpv created

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { less_than: 6, greater_than_or_equal_to: 0 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning database..."
+Restaurant.destroy_all
+
+puts "creating restaurants..."
+
+dominos = Restaurant.create!(name: "Domino's", address: "Everywhere", category: "italian")
+miss_pizza = Restaurant.create!(name: "Miss Pizza", address: "Şişhane, İstanbul", category: "italian")
+chinese_bufe = Restaurant.create!(name: "Çin Büfe", address: "Beşiktaş, İstanbul", category: "chinese")
+memo = Restaurant.create!(name: "The Memo", address: "Üsküdar, İstanbul", category: "belgian", phone_number: "252 545 54 23")
+brekkie = Restaurant.create!(name: "Brekkie", address: "Kadıköy, İstanbul", category: "french")
+
+puts "finished"
